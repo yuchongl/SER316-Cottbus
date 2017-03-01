ade26f98e7a330f1b916b3a80c1276cd44561a91
tdirusso <tdirusso@asu.edu>
2017-03-01 14:10:27 -0700

    Task #44 AND #45 done

--- src/net/sf/memoranda/ui/TaskTableModel.java
+++ src/net/sf/memoranda/ui/TaskTableModel.java
@@ -42,7 +42,7 @@ import java.util.Hashtable;
 public class TaskTableModel extends AbstractTreeTableModel implements TreeTableModel {
 
     String[] columnNames = {"", Local.getString("To-do"),
-            Local.getString("Start date"), Local.getString("End date"),
+            Local.getString("Start date"), Local.getString("Due date"),
             Local.getString("Priority"), Local.getString("Status"),
             "% " + Local.getString("done") };
 
