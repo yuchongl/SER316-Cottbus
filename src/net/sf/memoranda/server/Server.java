8e3a3d11986976cbc4e019fba798f9e99a624581
93@liyc.me <syncmaster93>
2017-03-26 02:32:37 -0700

    Added description and home page for HTTP server. Task #76

--- src/net/sf/memoranda/server/Server.java
+++ src/net/sf/memoranda/server/Server.java
@@ -18,10 +18,52 @@ public class Server
 	public static void main(String[] args) throws Exception {
 		HttpServer server = HttpServer.create(new InetSocketAddress(7501), 0);
 		server.createContext("/mem", new MemHttpHandler());
+		server.createContext("/", new DefaultHandler());
 		server.setExecutor(null);
 		server.start();
 		
 		System.out.println("http server started");
+	}
+	
+	static class DefaultHandler implements HttpHandler
+	{
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+			String response = "<html>";
+			
+			response += "<style type=\"text/css\"><!--.tab { margin-left: 40px; }--></style>";
+			
+			response += "<h2>SER316 memoranda project HTTP server. Task #76</h2>";
+			response += "<h3>Storage for memoranda notes</h3>";
+			response += "<div class=\"tab\">";
+			
+			response += "<p>This server uses HTTP query as parameters. It currently supports two operations: save note and read note. ";
+			response += "For notes operation, the format of the query is listed below:</p>";
+			response += "<code class=\"tab\">/mem?op=[notes_op]&action=[save | load]&title=[...]&content=[...]</code>";
+			response += "Note that the title of the note is used to identify it.";
+			
+			response += "<h3>Saving a note</h3>";
+			response += "<code class=\"tab\">Example: GET http://104.236.193.225:7501/mem?op=notes_op&action=save&title=test&content=test_content</code>";
+			response += "<p>responses \"OK\" on success.</p>";
+			
+			response += "<h3>Reading a note</h3>";
+			
+			response += "<code class=\"tab\">Example: GET http://104.236.193.225:7501/mem?op=notes_op&action=load&title=test</code>";
+			response += "<p>responses \"no such note\" if the note with the specified title does not exist.</p>";
+			response += "<p>responses the serialized content of the note if it is found.</p>";
+			response += "<code class=\"tab\">Example response: 2;5,6,title,test23;7,12,content,test_content;</code>";
+			response += "<p>To deserialize the note, using the following code: </p>";
+			response += "<code>NoteStorage ns = new NoteStorage();<br />ns.deserialize(str);</code>";
+			
+			response += "</div>";
+			response += "<hr /><small>Yuchong Li, Mar. 26, 2017</small>";
+			response += "</html>";
+			
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
 	}
 
 	static class MemHttpHandler implements HttpHandler
@@ -50,7 +92,7 @@ public class Server
 				NoteStorage ns = new NoteStorage();
 				ns.setTitle(noteTitle);
 				ns.setContent(noteContent);
-				
+			
 				// save it to the map
 				notes.put(noteTitle, ns);
 				
